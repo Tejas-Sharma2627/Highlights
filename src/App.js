0dd9cb91851c79259a80ea72ae601c5237efbc1f
Tejas Sharma <tejassharma_ee20a15_36@dtu.ac.in>
2022-04-16 13:19:50 +0530

    final part 2

--- src/App.js
+++ src/App.js
@@ -1,22 +1,76 @@
 import React, { useEffect, useState } from "react";
-
+import _ from "lodash";
 import NoteContainer from "./Components/NoteContainer/NoteContainer";
 import Sidebar from "./Components/Sidebar/Sidebar";
-import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import { DragDropContext } from "react-beautiful-dnd";
 import "./App.css";
-
+import { v4 } from "uuid";
+import Note from "./Components/Note/Note";
+import { Droppable, Draggable } from "react-beautiful-dnd";
+import { paginationItemClasses } from "@mui/material";
+import deleteIcon from "./assets/delete.svg";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import { AiOutlineEdit } from "react-icons/ai";
+import MenuItem from "@mui/material/MenuItem";
+import Button from "@mui/material/Button";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import "./Components/Note/Note.css";
+import plusIcon from "./assets/plus.png";
+const trialItem = {
+  id: v4(),
+  name: "Trial Item",
+};
+const trialItem2 = {
+  id: v4(),
+  name: "Trial Item2",
+};
 function App() {
-  const [notes, setNotes] = useState([]
-  );
-  const[buckets, setBuckets]= useState(["Trial"]);
-  const addBuckets=(bucket)=>{
-    const tempBuckets=[...buckets];
-    tempBuckets.push(bucket);
-    setBuckets(tempBuckets);
-  }
+  const [notes, setNotes] = useState([]);
+  const [buckets, setBuckets] = useState(["Trial"]);
+  const [text, setText] = useState("");
+  const colors = ["#fe9b72", "#fec971", " #00d4fe", "#b693fd", "#e4ee91"];
+  const [listOpen, setListOpen] = useState(false);
+  const [buck, setBuck] = useState(true);
+  const [bucketName, setBucketName] = useState("");
+  const [pair, setPair] = useState({
+    default: {
+      title: "Default Bucket",
+      items: [trialItem, trialItem2],
+    },
+
+    new: {
+      title: "New Bucket",
+      items: [],
+    },
+    "in-progress": {
+      title: "In Progress",
+      items: [],
+    },
+  });
+  const addBuckets = (bucket) => {
+    const newPair = { ...pair, [bucket]: { title: bucket, items: [] } };
+    setPair(newPair);
+  };
+
   const addNote = (color) => {
     const tempNotes = [...notes];
-
+    setPair((prev) => {
+      return {
+        ...prev,
+        default: {
+          title: "title",
+          items: [
+            {
+              id: v4(),
+              name: text,
+            },
+            ...prev.default.items,
+          ],
+        },
+      };
+    });
     tempNotes.push({
       id: Date.now() + "" + Math.floor(Math.random() * 78),
       bucket: "",
@@ -26,7 +80,7 @@ function App() {
     });
     setNotes(tempNotes);
   };
-  
+
   const deleteNote = (id) => {
     const tempNotes = [...notes];
 
@@ -50,16 +104,122 @@ function App() {
   useEffect(() => {
     localStorage.setItem("notes-app", JSON.stringify(notes));
   }, [notes]);
+  const handleDragEnd = ({ destination, source }) => {
+    if (!destination) {
+      return;
+    }
 
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+    const itemCopy = { ...pair[source.droppableId].items[source.index] };
+    setPair((prev) => {
+      prev = { ...prev };
+      // Remove from previous items array
+      prev[source.droppableId].items.splice(source.index, 1);
+
+      // Adding to new items array location
+      prev[destination.droppableId].items.splice(
+        destination.index,
+        0,
+        itemCopy
+      );
+
+      return prev;
+    });
+  };
+  const handleChange = (event) => {
+    setBucketName(event.target.value);
+
+    setBuck(false);
+  };
+  const addItem = (texte) => {
+    setPair((prev) => {
+      return {
+        ...prev,
+        default: {
+          title: "Todo",
+          items: [
+            {
+              id: v4(),
+              name: texte,
+            },
+            ...prev.default.items,
+          ],
+        },
+      };
+    });
+
+    setText("");
+  };
   return (
     <div className="App">
-      <Sidebar addNote={addNote} addBuckets={addBuckets} buckets = {buckets} />
-      <NoteContainer
-        notes={notes}
-        buckets={buckets}
-        deleteNote={deleteNote}
+      <Sidebar
+        addNote={addNote}
+        addItem={addItem}
         updateText={updateText}
+        addBuckets={addBuckets}
+        buckets={buckets}
       />
+
+      <DragDropContext onDragEnd={handleDragEnd}>
+        {_.map(pair, (data, key) => {
+          return (
+            <div key={key}>
+              <h3>{data.title}</h3>
+              <Droppable droppableId={key}>
+                {(provided, snapshot) => {
+                  return (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.droppableProps}
+                      className={"droppable-col"}
+                    >
+                      <div className="note-container">
+                        {data.items.map((el, index) => {
+                          return (
+                            <Draggable
+                              key={el.id}
+                              index={index}
+                              draggableId={el.id}
+                            >
+                              {(provided, snapshot) => {
+                                console.log(snapshot);
+                                return (
+                                  <div
+                                    className={`item ${
+                                      snapshot.isDragging && "dragging"
+                                    }`}
+                                    ref={provided.innerRef}
+                                    {...provided.draggableProps}
+                                    {...provided.dragHandleProps}
+                                  >
+                                    <Button
+                                      variant="contained"
+                                      
+                                    >
+                                      {data.title}
+                                    </Button>
+                                    {el.name}
+                                  </div>
+                                );
+                              }}
+                            </Draggable>
+                          );
+                        })}
+                      </div>
+                      {provided.placeholder}
+                    </div>
+                  );
+                }}
+              </Droppable>
+            </div>
+          );
+        })}
+      </DragDropContext>
     </div>
   );
 }
--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -5,11 +5,66 @@ import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import "./Sidebar.css";
-
+import deleteIcon from "../../assets/delete.svg";
+import InputLabel from "@mui/material/InputLabel";
+import { AiOutlineEdit } from "react-icons/ai";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+let timer = 500,
+  timeout;
 function Sidebar(props) {
+  const formatDate = (value) => {
+    if (!value) return "";
+
+    const date = new Date(value);
+    const monthNames = [
+      "Jan",
+      "Feb",
+      "March",
+      "April",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sept",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+
+    let hrs = date.getHours();
+    let amPm = hrs >= 12 ? "PM" : "AM";
+    hrs = hrs ? hrs : "12";
+    hrs = hrs > 12 ? (hrs = 24 - hrs) : hrs;
+
+    let min = date.getMinutes();
+    min = min < 10 ? "0" + min : min;
+
+    let day = date.getDate();
+    const month = monthNames[date.getMonth()];
+
+    return `${hrs}:${min} ${amPm} ${day} ${month}`;
+  };
+
+  const debounce = (func) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(func, timer);
+  };
+
+  const updateText = (text, id) => {
+    debounce(() => props.updateText(text, id));
+  };
+  const [buck, setBuck] = useState(true);
+  const[text, setText] = useState("");
+  const [bucketName, setBucketName] = useState("");
+  const handleChange = (event) => {
+    setBucketName(event.target.value);
+    console.log(props.bucket);
+    setBuck(false);
+  };
   const colors = ["#fe9b72", "#fec971", " #00d4fe", "#b693fd", "#e4ee91"];
   const [listOpen, setListOpen] = useState(false);
-  const [bucketName, setBucketName] = useState("");
   return (
     <div className="sidebar">
       <div>
@@ -32,6 +87,52 @@ function Sidebar(props) {
           Add Bucket
         </Button>
       </div>
+      <div className="note" style={{ backgroundColor: colors[Math.floor(Math.random() * 6) + 1] }}>
+      {buck && (
+        <Box sx={{ minWidth: 120 }}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Bucket</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={bucketName}
+              label="Bucket"
+              onChange={handleChange}
+            >
+              
+                <MenuItem value={"Default"}>Default</MenuItem>
+                <MenuItem value={"New Bucket"}>New Bucket</MenuItem>
+                <MenuItem value={"In Progress"}>In Progress</MenuItem>
+              
+            </Select>
+          </FormControl>
+        </Box>
+      )}
+      {!buck && (
+        <div style={{ display: "flex" }}>
+          <Button variant="contained" style={{ width: "150px" }}>
+            {bucketName}
+          </Button>
+          <AiOutlineEdit
+            style={{
+              color: "white",
+              backgroundColor: "black",
+              fontSize: "20px",
+              borderRadius: "5px",
+              marginLeft: "50px",
+            }}
+            onClick={() => {
+              setBuck(true);
+            }}
+          ></AiOutlineEdit>
+        </div>
+      )}
+      <textarea
+        className="note_text"
+        defaultValue={text}
+        onChange={(event) => setText(event.target.value)}
+      />
+    </div>
       <div>
         <img src={plusIcon} alt="Add" onClick={() => setListOpen(!listOpen)} />
         <ul className={`sidebar_list ${listOpen ? "sidebar_list_active" : ""}`}>
@@ -40,7 +141,7 @@ function Sidebar(props) {
               key={index}
               className="sidebar_list_item"
               style={{ backgroundColor: item }}
-              onClick={() => props.addNote(item)}
+              onClick={()=>props.addItem(text)}
             />
           ))}
         </ul>
